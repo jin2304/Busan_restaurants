eecd960895a93f7d77d581e1e6f2f520f2ad9006
jin2304 <k1017k2@naver.com>
2024-04-28 00:43:01 +0900

    modify history

--- src/main/java/com/web/store/controller/TemplateController.java
+++ src/main/java/com/web/store/controller/TemplateController.java
@@ -13,11 +13,14 @@ public class TemplateController {
     public String template() {
         return "template/fragment/fragmentMain";
     }
+<<<<<<< HEAD
 
     @GetMapping("/layout")
     public String layout() {
         return "template/layout/layoutMain";
     }
+=======
+>>>>>>> 7d6d099 (v2 add TemplateController)
 }
 
 
