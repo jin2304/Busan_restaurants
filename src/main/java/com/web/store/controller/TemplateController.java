a3bcd318c96e08b28daa04ae407a5a3c48d76820
jin2304 <k1017k2@naver.com>
2024-04-27 23:35:14 +0900

    v2.2 add layout

--- src/main/java/com/web/store/controller/TemplateController.java
+++ src/main/java/com/web/store/controller/TemplateController.java
@@ -13,6 +13,11 @@ public class TemplateController {
     public String template() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
 }
 
 
